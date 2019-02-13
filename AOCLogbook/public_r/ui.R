ba2dbd96048cc0b967660247e806b25653fd23fb
russellrl19 <39773459+russellrl19@users.noreply.github.com>
2019-02-13 09:57:32 -0500

    notification examples
    -examples only

--- AOCLogbook/public_r/ui.R
+++ AOCLogbook/public_r/ui.R
@@ -17,7 +17,26 @@ ui <- fluidPage(
   tags$head(tags$script(src = "message-handler.js")), 
   dashboardPage(
     skin = "green",
-    dashboardHeader(title = "VMI Green Book"),
+    dashboardHeader(title = "VMI Green Book",
+      dropdownMenu(type = "messages",
+        messageItem(
+        from = "Sales Dept",
+        message = "Sales are steady this month."
+      ),
+      messageItem(
+        from = "New User",
+        message = "How do I register?",
+        icon = icon("question"),
+        time = "13:45"
+      ),
+      messageItem(
+        from = "Support",
+        message = "The new server is ready.",
+        icon = icon("life-ring"),
+        time = "2014-12-01"
+      )
+     )
+    ),
     dashboardSidebar(
       sidebarMenu(
         menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
